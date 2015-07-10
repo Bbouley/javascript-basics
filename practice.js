29bc57d6525658e92d47148e8cc3dec83aedd8f2
bbouley <b.bouley@hotmail.co.uk>
2015-07-10 09:39:20 -0600

    added age question and response in js file

--- practice.js
+++ practice.js
@@ -1,3 +1,7 @@
 var name = prompt('What is your name?');
 
 alert ('Hello '+ name + '!');
+
+var age = prompt('And how old are you?');
+
+alert ('Your name is ' + name + ' and you are ' + age + ' years old!')
